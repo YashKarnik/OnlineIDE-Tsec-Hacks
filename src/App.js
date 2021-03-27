11d3e38df08d66e62c694129b46094dda168d5ef
Yash Karnik <55018090+YashKarnik@users.noreply.github.com>
2021-03-27 21:43:37 +0530

    Fix: route paramenters

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import Login from './compnenets/Login';
 import { UserProvider } from './contexts/userAuthContext';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import PrivateRoute from './PrivateRoute/privateRoute';
-import Ide from './compnenets/ide';
+import Ide from './compnenets/ide/ide';
+import Files from './compnenets/files/files';
+
 export default function App() {
 	return (
 		<UserProvider>
 			<Router>
 				<Route exact path='/' component={Login} />
-				<PrivateRoute exact path='/ide' component={Ide} />
+				<PrivateRoute exact path='/:filename/ide' component={Ide} />
+				<PrivateRoute exact path='/files' component={Files} />
 			</Router>
 		</UserProvider>
 	);
