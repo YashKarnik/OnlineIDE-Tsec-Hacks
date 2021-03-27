a3f2f7bfdfe19bdbf183eb051dbf20f954861827
Yash Karnik <55018090+YashKarnik@users.noreply.github.com>
2021-03-27 21:45:40 +0530

    Add file support

---
+++ src/compnenets/files/CreateNewFileForm.jsx
@@ -0,0 +1,86 @@
+import React, { useEffect } from 'react';
+import creator from 'name-creator';
+import { useUserContext } from '../../contexts/userAuthContext';
+import { db } from '../../firebase';
+
+export default function CreateNewFileForm({
+	setCreatenewFile,
+	newFile,
+	setNewFile,
+}) {
+	const { currentUser } = useUserContext();
+
+	useEffect(() => {
+		setNewFile({
+			fileName: creator.generate({ number: true }).dashed,
+			date: String(new Date()).substring(0, 16),
+			type: 'Python',
+		});
+		return () => setNewFile('');
+	}, []);
+	async function handleSubmit(e) {
+		e.preventDefault();
+		console.log(newFile);
+		db.doc(`${currentUser.uid}/${newFile.fileName}`)
+			.set({
+				fileName: newFile.fileName,
+				date: newFile.date,
+				type: newFile.type,
+				value: '',
+			})
+			.then(e => window.location.reload());
+	}
+	return (
+		<form className='new-file-form'>
+			<div className='new-file-form-inputs'>
+				<label htmlFor=''>Filename</label>
+				<input
+					placeholder='Filename'
+					value={newFile.fileName}
+					onChange={e => {
+						console.log(newFile, e.target.value);
+						setNewFile(p => {
+							return { ...p, ['fileName']: e.target.value };
+						});
+					}}
+					type='text'
+				/>
+			</div>
+			<div className='new-file-form-inputs'>
+				<label htmlFor=''>Date</label>
+				<input
+					disabled={true}
+					placeholder='Filename'
+					value={String(newFile.date)}
+					type='text'
+				/>
+			</div>
+			<div className='new-file-form-inputs'>
+				<label htmlFor=''>Language</label>
+				<select
+					name='cars'
+					onClick={e => {
+						console.log(newFile);
+						setNewFile(p => {
+							return { ...p, ['type']: e.target.value };
+						});
+					}}
+					id='cars'>
+					<option value='Python'>Python</option>
+					<option value='Javascript'>Javascript</option>
+				</select>
+			</div>
+
+			<button
+				className='close-file-form'
+				style={{ borderRadius: '0', outline: 'none' }}
+				onClick={handleSubmit}>
+				{' '}
+				✓
+			</button>
+			<div className='close-file-form' onClick={() => setCreatenewFile(false)}>
+				&times;
+			</div>
+		</form>
+	);
+}
---
+++ src/compnenets/files/files.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import FileRowComponent from './fileRowComponent';
+import CreateNewFileForm from './CreateNewFileForm';
+import Navbar from '../Navbar';
+import { useUserContext } from '../../contexts/userAuthContext';
+import { db } from '../../firebase';
+export default function Files() {
+	const { currentUser } = useUserContext();
+	const [filesList, setFilesList] = useState([]);
+	const [createnewFile, setCreatenewFile] = useState(false);
+	const [newFile, setNewFile] = useState({ fileName: '', type: '', date: '' });
+	useEffect(async () => {
+		const citiesRef = db.collection(currentUser.uid);
+		const snapshot = await citiesRef.get();
+		snapshot.forEach(doc => {
+			setFilesList(p => [...p, doc.data()]);
+		});
+		return () => setFilesList([]);
+	}, []);
+
+	function handleClick(e) {
+		setCreatenewFile(true);
+	}
+	return (
+		<>
+			<Navbar />
+			<div className='files-container'>
+				<h3 className='text-center'>{currentUser.displayName}'s Files</h3>
+				<ul>
+					{filesList.map((e, i) => (
+						<Link
+							style={{ color: 'black', textDecoration: 'none' }}
+							to={`/${e.fileName}/ide`}>
+							<FileRowComponent
+								key={i}
+								fileName={e.fileName}
+								date={e.date || String(new Date()).substring(0, 16)}
+								type={e.type}
+							/>
+						</Link>
+					))}
+				</ul>
+				<div
+					className='add-new-file'
+					style={{ display: createnewFile && 'none' }}
+					onClick={handleClick}>
+					+
+				</div>
+				{createnewFile && (
+					<CreateNewFileForm
+						setCreatenewFile={setCreatenewFile}
+						newFile={newFile}
+						setNewFile={setNewFile}
+					/>
+				)}
+			</div>
+		</>
+	);
+}
